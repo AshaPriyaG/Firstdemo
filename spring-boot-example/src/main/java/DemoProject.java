83a3522a1241d1a1d354fc7fc318d03f91058d19
lenovo <lenovo@LAPTOP-8TG7ILDF>
2021-08-16 15:58:42 +0530

    Commit!!

--- spring-boot-example/src/main/java/DemoProject.java
+++ spring-boot-example/src/main/java/DemoProject.java
@@ -1,5 +1,7 @@
 public class DemoProject {
 	    public static void main(String[] args) {
 	        System.out.println("Hello, World!"); 
+	        System.out.println("GitHub!");
+	        System.out.println("Example!");
 	    }
 }
