6cfebea67d598943d3c92f76d4026016109f5c91
lenovo <lenovo@LAPTOP-8TG7ILDF>
2021-08-17 12:08:05 +0530

    Added display165

--- spring-boot-example/src/main/java/prog1.java
+++ spring-boot-example/src/main/java/prog1.java
@@ -2,6 +2,9 @@ public class prog1 {
 	public static void dIsplay() {
 		System.out.println("Display"); 
 	}
+	public static void dIsplay165() {
+		System.out.println("Display165"); 
+	}
 	public static void main(String[] args) {
         System.out.println("Program2"); 
     }
