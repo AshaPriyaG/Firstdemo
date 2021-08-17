096fbe92d9f6b05eead9fa672fe4e099978e39f3
lenovo <lenovo@LAPTOP-8TG7ILDF>
2021-08-17 11:52:28 +0530

    sum

--- spring-boot-example/src/main/java/prog1.java
+++ spring-boot-example/src/main/java/prog1.java
@@ -1,4 +1,7 @@
 public class prog1 {
+	public static void Sum(){
+		System.out.println("sum program");
+	}
 	public static void main(String[] args) {
         System.out.println("Program2"); 
     }
