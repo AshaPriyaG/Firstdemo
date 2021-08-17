85583324d42d612bebb71944c5c5f4fd1a72f869
lenovo <lenovo@LAPTOP-8TG7ILDF>
2021-08-17 11:54:38 +0530

    Display

--- spring-boot-example/src/main/java/prog1.java
+++ spring-boot-example/src/main/java/prog1.java
@@ -1,4 +1,7 @@
 public class prog1 {
+	public static void dIsplay() {
+		System.out.println("Display"); 
+	}
 	public static void main(String[] args) {
         System.out.println("Program2"); 
     }
